09d7668a1942f39e6e2f522bb0a8b1c26bd26ed5
DaShaun <dashaun@redislabs.com>
2021-10-23 22:31:39 -0500

    .id

--- azurerm_resource_group_template_deployment.tf
+++ azurerm_resource_group_template_deployment.tf
@@ -4,8 +4,8 @@ resource "azurerm_resource_group_template_deployment" "acre_cluster" {
   deployment_mode     = var.acre_deployment_mode
   template_content = templatefile(var.acre_template_path_1,
     {
-      acre_name_1          = format("%s-%s", random_string.acre_group_name, var.primary-region),
-      acre_name_2          = format("%s-%s", random_string.acre_group_name, var.secondary-region),
+      acre_name_1          = format("%s-%s", random_string.acre_group_name.id, var.primary-region),
+      acre_name_2          = format("%s-%s", random_string.acre_group_name.id, var.secondary-region),
       location             = var.primary-region,
       location_2           = var.secondary-region
       acre_sku             = var.acre_sku,
@@ -13,7 +13,7 @@ resource "azurerm_resource_group_template_deployment" "acre_cluster" {
       acre_client_protocol = var.acre_client_protocol,
       acre_eviction_policy = var.acre_eviction_policy,
       acre_cluster_policy  = var.acre_cluster_policy,
-      acre_group_nickname  = random_string.acre_group_name,
+      acre_group_nickname  = random_string.acre_group_name.id,
       subscription_id      = data.azurerm_subscription.current.subscription_id,
       resource_group_name  = random_string.resource_group_name.id,
   })
